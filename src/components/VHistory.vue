dae0847eb506792ff8c2eb37e7531759bacbcd60
kchaw1 <heatarin@gmail.com>
2022-06-09 03:14:43 +0900

    fix: history api 화면 수정

--- src/components/VHistory.vue
+++ src/components/VHistory.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="article-preview">
-    <h1 v-text="history.title" />
-    <p v-text="history.description" />
+    <div class="article-meta">
+      <div class="info">
+        <span class="date">
+          {{ history.occurredAt.replace("T", " ") }}
+        </span>
+      </div>
+      <button
+        class="btn btn-sm pull-sm-right"
+        :class="{
+          'btn-primary': history.type === 'CREATE',
+          'btn-info': history.type === 'UPDATE',
+          'btn-second': history.type === 'DELETE'
+        }"
+        v-text="history.type"
+      />
+    </div>
+    <h2 v-text="history.title" />
+    <div v-text="history.description" />
+    <div
+      v-text="history.body"
+      :title="history.body"
+      style="font-size: 0.8rem; overflow: hidden; text-overflow: ellipsis; white-space:now-rap;"
+    />
   </div>
 </template>
 
